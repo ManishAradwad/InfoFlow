1eaf821dd0a0f7b7e7f74dc0a487ed63829eae2b
Manish Aradwad <manisharadwad@gmail.com>
2023-10-23 06:13:32 +0000

    fixed main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from llama_index import VectorStoreIndex, SimpleDirectoryReader
 
+print("Loading website...")
 documents = SimpleDirectoryReader('output_pdf').load_data()
 index = VectorStoreIndex.from_documents(documents)
 query_engine = index.as_query_engine()
-response = query_engine.query("How to use parametrized type and functions in Mojo?")
-print(response)
+print("Done!")
+
+while True:
+    print("=====================================")
+    user_input = input("\nEnter your query or Enter 'q' to quit: ")
+    if user_input == 'q' or user_input == '':
+        print("\nBye!")
+        break
+    response = query_engine.query(user_input)
+    print("\n", response)
+    
